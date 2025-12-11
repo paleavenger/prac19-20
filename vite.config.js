a7e0faee9bf21b689ff5b422d0014273f09746f7
paleavenger <lyrhg79@gmail.com>
2025-12-11 18:52:28 +0300

    Complete-Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: 'https://github.com/paleavenger/prac19-20'
+  base: '/prac19-20/'
 })
